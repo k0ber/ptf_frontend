725e8f3842f4c825ede2e1fc9a59890c2b4b1652
nik.brom <kober55@yandex.ru>
2026-05-04 22:00:07 +0300

    fix koin graph test

--- composeApp/src/jvmTest/kotlin/KoinGraphTest.kt
+++ composeApp/src/jvmTest/kotlin/KoinGraphTest.kt
@@ -1,58 +1,37 @@
-import com.russhwolf.settings.Settings
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.HttpClientEngineFactory
-import kotlinx.coroutines.CoroutineScope
+import io.mockk.junit4.MockKRule
+import io.mockk.mockk
+import org.junit.Rule
 import org.junit.Test
 import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.module
 import org.koin.test.KoinTest
-import org.koin.test.verify.verifyAll
+import org.koin.test.verify.verify
 import org.patifiner.client.ApiConfig
 import org.patifiner.client.KoinAppConfig
-import org.patifiner.client.NetworkObserver
-import org.patifiner.client.appModule
-import org.patifiner.client.root.RootNavigator
-import org.patifiner.client.root.login.data.AuthRepository
-import org.patifiner.client.root.login.data.SessionManager
-import org.patifiner.client.root.login.data.SessionStorage
-import org.patifiner.client.root.main.MainNavigator
-import org.patifiner.client.root.main.intro.IntroNavigator
-import org.patifiner.client.root.main.intro.user.UserInteractor
-import org.patifiner.client.root.main.mainModule
-import org.patifiner.client.root.rootModule
+import org.patifiner.client.prepareModulesFromConfig
 
 // koin verifyAll(..) requires jvm, so we can't put it in common tests
 class CheckModulesTest : KoinTest {
 
+    @get:Rule
+    val mockkRule = MockKRule(this)
+
     @OptIn(KoinExperimentalAPI::class)
     @Test
     fun checkAllModules() {
-        listOf(
-            appModule,
-            rootModule,
-            mainModule
-        ).verifyAll(
+        val testConfig = KoinAppConfig(
+            engine = mockk(relaxed = true),
+            apiConfig = ApiConfig(baseUrl = "http://localhost", port = 8080),
+            appScope = mockk(relaxed = true),
+            isDev = true
+        )
+
+        val allAppModules = prepareModulesFromConfig(testConfig)
+
+        module { includes(allAppModules) }.verify(
             extraTypes = listOf(
-                HttpClientEngineFactory::class,
-                HttpClientEngine::class,
-                ApiConfig::class,
-                CoroutineScope::class,
-                Settings::class,
-                NetworkObserver::class,
-                HttpClient::class,
-                KoinAppConfig::class,
-
-                AuthRepository::class,
-                SessionStorage::class,
-                SessionManager::class,
-
-                RootNavigator::class,
-                MainNavigator::class,
-                IntroNavigator::class,
-
-                UserInteractor::class,
+                Boolean::class, // for config constructor
             )
         )
     }
-
 }
